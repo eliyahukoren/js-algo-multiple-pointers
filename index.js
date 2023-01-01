b1ede1a8392805fd73fe39b49415913d4319ff51
eliyahukoren <elik.koren@gmail.com>
2023-01-01 11:56:50 +0200

    add some examples

--- index.js
+++ index.js
@@ -1,5 +1,82 @@
-const areThereDuplicates = function(){
+// [1, 2, 2]
+//  i
+// [1, 4, 5, 6, 2, 4, 1, 5, 8]
+// [1, 1, 2, 4, 4, 5, 5, 6, 8]
+//     j 
+const areThereDuplicates = function(...args){
+	if (args.length === 0) return false;
+	
+	args.sort((a, b) => a > b);
 
+	let i = 0;
+	let j = 1;
+
+	while(j < args.length){
+		if( args[i] === args[j]) return true;
+
+		i ++;
+		j ++;
+	}
+
+	return false;
 };
 
-module.exports = { areThereDuplicates }
+const areThereDuplicatesOneLiner = function(){
+	return new Set(arguments).size !== arguments.length;
+}
+
+
+//  i
+// [1, 1, 1, 1, 1, 2]
+//     j
+// Multiple Pointers started from the left side of array
+const countUniqueValues = (arr) => {
+	if( arr.length == 0) return 0;
+
+	let i = 0;
+
+	for (let j = 1; j < arr.length; j++){
+		if(arr[i] !== arr[j]){
+			i++;
+			arr[i] = arr[j];
+		}
+	}
+
+	// finally return number of unique integers
+	return i + 1;
+};
+
+
+// Multiple pointers, head <-> tail pairs
+// Given sorted array of integers.
+// Return an array that includes both values that sum to zero or undefined if pair
+// does not exist
+const sumZero = (arr) => {
+	if (arr.length === 0) return undefined;
+
+	let left = 0;
+	let right = arr.length - 1;
+
+	while(left < right){
+		let sum = arr[left] + arr[right]
+		// we found zero sum, return 
+		if( sum === 0){
+			return [arr[left], arr[right]];
+		}
+
+		if (sum > 0){
+			// move right to the left side
+			right --;
+		}else{
+			// move left to the right side
+			left ++;
+		}
+	}
+}
+
+module.exports = {
+	areThereDuplicates,
+	areThereDuplicatesOneLiner,
+	countUniqueValues,
+	sumZero,
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,23 +1,107 @@
-const { areThereDuplicates } = require("../index");
+const { areThereDuplicates, areThereDuplicatesOneLiner, countUniqueValues, sumZero } = require("../index");
 
-describe("Testing Are There Duplicates - Multiple Pointers", () => {
+describe("Are There Duplicates - Multiple Pointers", () => {
 	describe("Test if function is exists", () => {
 		it("Function exists", () => {
 			expect(areThereDuplicates).toBeDefined();
 		});
 	});
 
-	describe("Duplicates", () => {
-		it("Test duplicates is exists", () => {
-			expect(areThereDuplicates(1, 2, 2)).toEqual(true);
-			expect(areThereDuplicates("a", "b", "c", "a")).toEqual(true);
+	describe("Unit tests", () => {
+		const data = [
+			["1, 2, 3", true, [1, 2, 2]],
+			// ['"a", "b", "c", "a"', true, ["a", "b", "c", "a"]],
+			["1, 2, 3", false, [1, 2, 3]],
+			['"a", "b", "c", "d"', false, ["a", "b", "c", "d"]],
+		];
+
+		test.each(data)(
+			"areThereDuplicates( %s ), should return %s",
+			(desc, expected, given) => {
+				expect(areThereDuplicates(...given)).toEqual(expected);
+			}
+		);
+	});
+
+});
+
+describe("Are There Duplicates - One Liner", () => {
+	describe("Test if function is exists", () => {
+		it("Function exists", () => {
+			expect(areThereDuplicatesOneLiner).toBeDefined();
 		});
 	});
 
-	describe("Duplicates", () => {
-		it("Test duplicates is not exists", () => {
-			expect(areThereDuplicates(1, 2, 3)).toEqual(false);
-			expect(areThereDuplicates("a", "b", "c", "d")).toEqual(false);
+	describe("Unit tests", () => {
+		const data = [
+			["1, 2, 3", true, [1, 2, 2]],
+			['"a", "b", "c", "a"', true, ["a", "b", "c", "a"]],
+			["1, 2, 3", false, [1, 2, 3]],
+			['"a", "b", "c", "d"', false, ["a", "b", "c", "d"]],
+		];
+
+		test.each(data)(
+			"areThereDuplicatesOneLiner( %s ), should return %s",
+			(desc, expected, given) => {
+				expect(areThereDuplicatesOneLiner(...given)).toEqual(expected);
+			}
+		);
+	});
+});
+
+
+describe("Count Unique Values - Multiple Pointers", () => {
+	describe("Test if function is exists", () => {
+		it("Function exists", () => {
+			expect(countUniqueValues).toBeDefined();
 		});
+	});
+
+	describe("Unique Values", () => {
+		const table = [
+			[[1, 1, 1, 1, 1, 2], 2],
+			[[1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13], 7],
+			[[-2, -1, -1, 0, 1], 4],
+			[[], 0],
+		];
+
+		test.each(table)(
+			"With %j countUniqueValues returns %i",
+			(arr, expected) => {
+				expect(countUniqueValues(arr)).toEqual(expected);
+			}
+		);
+
+		test.each`
+			arr          | expected
+			${[1, 2, 3]} | ${3}
+			${[2, 2, 3]} | ${2}
+		`("Returns $expected when inspect $arr", ({ arr, expected }) => {
+			expect(countUniqueValues(arr)).toEqual(expected);
+		});
+	});
+});
+
+describe("Sum Zero - Multiple Pointers", () => {
+	describe("Test if function is exists", () => {
+		it("Function exists", () => {
+			expect(sumZero).toBeDefined();
+		});
+	});
+
+	describe("Zero pairs", () => {
+		test.each`
+			arr                            | expected
+			${[-3, -2, -1, 0, 1, 2, 3]}    | ${[-3, 3]}
+			${[-3, -2, -1, 0, 1, 2, 3, 5]} | ${[-3, 3]}
+			${[-3, -2, -1, 0, 1, 2, 3, 2]} | ${[-2, 2]}
+			${[-2, 0, 1, 3]}               | ${undefined}
+			${[1, 2, 3, 4]}                | ${undefined}
+		`(
+			"sumZero returns $expected when inspect $arr",
+			({ arr, expected }) => {
+				expect(sumZero(arr)).toEqual(expected);
+			}
+		);
 	});
 });
